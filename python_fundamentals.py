8f693b746f16c138e57edf5bb31f3723acd37482
shalonningram1 <mscianika@gmail.com>
2023-06-21 11:37:49 -0400

    adding files

---
+++ python_fundamentals.py
@@ -0,0 +1,27 @@
+
+sum_scores = []
+total_sum = 0
+
+for i in range(1, 6):
+    scores = int(input("Enter test score: "))
+
+    if scores > 100 or scores < 0:
+                raise ValueError("Out of range")
+    sum_scores.append(scores)
+    
+
+for score in sum_scores:
+    total_sum += score
+
+average_score = int(total_sum/len(sum_scores))
+
+if average_score > 90:
+    print("Your Final Grade is A")
+elif average_score <= 89 and average_score >= 80:
+    print("Your Final Grade is B")
+elif average_score <= 79 and average_score >= 70:
+    print("Your Final Grade is C")
+elif average_score <= 69 and average_score >= 60:
+    print("Your Final Grade is D")
+else:
+    print("Your Final Grade is F")
